ce12e19d6152f126739cc8bfd9490f31f2b1154a
Nikolarchitect <nikolarchitect@gmail.com>
2022-10-09 18:49:10 +0500

    initialization

---
+++ concurrency/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  concurrency
+//
+//  Created by Nikolay Kiyko on 09.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .systemGray4
+    }
+}
+
